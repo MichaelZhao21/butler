18b582a560b1c3d930bd976b655c15be23180060
Michael Zhao <michaelzhao314@gmail.com>
2022-08-21 12:59:50 -0500

    Added xkcd comic

--- src/functions/daily.ts
+++ src/functions/daily.ts
@@ -1,25 +1,39 @@
+import { userMention } from '@discordjs/builders';
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { Client, MessageEmbed } from 'discord.js';
 import fetch from 'node-fetch';
 import { getUser } from '../util';
+
+dayjs.extend(relativeTime);
 
 export async function runDailyTask(client: Client) {
     // Get the news
     const newsEmbed = new MessageEmbed()
         .setColor('#e5c914')
         .setTitle('News for ' + dayjs().format('dddd, MMMM D, YYYY'));
-    const news = await fetch('https://api.michaelzhao.xyz/news').then((data) => data.json());
+    const news = await fetch(
+        `https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=${process.env.NEWS_API}`
+    ).then((data) => data.json());
     news.articles.forEach((newsItem) => {
         newsEmbed.addField(
             newsItem.title.substring(0, 256),
-            `[${newsItem.source.name}] ${newsItem.url}`
+            `[${dayjs(newsItem.publishedAt).fromNow()}] ${newsItem.url}`
         );
     });
 
+    const comic = await fetch('https://xkcd.com/info.0.json').then((data) => data.json());
+    const xkcdEmbed = new MessageEmbed()
+        .setImage(comic.img)
+        .setColor('AQUA')
+        .setTitle('XKCD Comic of the Day');
+
     // Send all embeds
     const user = await getUser(client);
-    user.send({ embeds: [newsEmbed] }).catch((err) => {
+    try {
+        user.send({ embeds: [newsEmbed, xkcdEmbed] });
+    } catch (err) {
         console.error(err);
         user.send(err);
-    });
+    }
 }
